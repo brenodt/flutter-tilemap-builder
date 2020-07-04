bac2915bfc854c209921de4287c060b609c7fc48
Breno Teodoro <54450520+brenodt@users.noreply.github.com>
2020-07-04 20:40:20 -0300

    #1 Add Title to inspector section and Create/Open buttons

--- tilemap_builder/lib/src/inspector.dart
+++ tilemap_builder/lib/src/inspector.dart
@@ -7,6 +7,47 @@ class Inspector extends StatelessWidget {
     return Container(
       width: _screenWidth * 0.3,
       color: Colors.purple,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: <Widget>[
+            Text(
+              'Inspector',
+              style: Theme.of(context).textTheme.headline6,
+            ),
+            Expanded(child: Container()),
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: RaisedButton(
+                    onPressed: () {},
+                    elevation: 0.0,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(18.0),
+                    ),
+                    child: const Text('Create map'),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: RaisedButton(
+                    onPressed: () {},
+                    elevation: 0.0,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(18.0),
+                    ),
+                    child: const Text('Open map'),
+                  ),
+                ),
+              ],
+            ),
+            Expanded(child: Container()),
+          ],
+        ),
+      ),
     );
   }
 }
