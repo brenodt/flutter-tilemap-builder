fe52b2c4f310fbcba07b241d8c1eb0bbc0d86e1f
Breno Teodoro <54450520+brenodt@users.noreply.github.com>
2020-07-04 19:29:25 -0300

    #1 Create main App and widgets

---
+++ tilemap_builder/lib/src/map.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class TileMap extends StatefulWidget {
+  @override
+  _TileMapState createState() => _TileMapState();
+}
+
+class _TileMapState extends State<TileMap> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
