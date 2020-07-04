536d6595df9dcbd1a26db33cdeb075d168ce329e
Breno Teodoro <54450520+brenodt@users.noreply.github.com>
2020-07-04 20:17:33 -0300

    #1 Add zoom control buttons

--- tilemap_builder/lib/src/map.dart
+++ tilemap_builder/lib/src/map.dart
@@ -10,6 +10,69 @@ class _TileMapState extends State<TileMap> {
   Widget build(BuildContext context) {
     return Container(
       color: Colors.blueAccent,
+      child: Stack(
+        alignment: Alignment.bottomCenter,
+        children: <Widget>[
+          Column(
+            children: <Widget>[
+              Expanded(
+                child: Container(),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const <Widget>[
+                  // TODO(brenodt): Link UI with functions to manipulate map.
+                  CircularButton(
+                    onTap: null,
+                    size: 30.0,
+                    icon: Icons.zoom_out,
+                  ),
+                  Slider(
+                    min: 0.0,
+                    max: 100.0,
+                    value: 0.0,
+                    onChanged: null,
+                  ),
+                  CircularButton(
+                    onTap: null,
+                    size: 30.0,
+                    icon: Icons.zoom_in,
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CircularButton extends StatelessWidget {
+  const CircularButton({
+    this.onTap,
+    this.size,
+    this.icon,
+  });
+  final IconData icon;
+  final VoidCallback onTap;
+  final double size;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: ClipOval(
+        child: Container(
+          width: size,
+          height: size,
+          color: Colors.black26,
+          child: Icon(
+            icon,
+            color: Colors.white70,
+          ),
+        ),
+      ),
     );
   }
 }
